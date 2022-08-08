658ecd62c1406876ed596eb7cf4d6ba542dd54d3
theopharnell <98174756+theopharnell@users.noreply.github.com>
2022-08-08 05:53:26 -0300

    desafio array

--- Js/script.js
+++ Js/script.js
@@ -1,74 +1,144 @@
-// Ejemplo N°1 (ciclo for)
+// DESAFIO ARRAYS
 
-for (let i = 10; i >= 1; i--) {
-    console.log(`iteracion N° ${i}`);
-}
-console.log("Fin del ciclo");
+const arreglo = [10,20,30];
+console.log(arreglo);
 
-// Ejemplo N°2 (ciclo dentro de otro ciclo)
-
-for (let i = 1; i <= 5; i++) {
-    console.log(`Empieza la iteracion N° ${i}`);
-    for (let j = 1; j < 6; j++) {
-        console.log(j);       
-    }    
-}
-console.log("Fin del ciclo");
+// ARRAY con "new":
+const meses = new Array("enero","febrero","marzo");
+console.log(meses);
 
 
-// Ejemplo N°3 (ciclo while)
+// ARRAY con multiple elementos:
+const varios = [10,"hola",true,[10,20,30]];
+console.log(varios);
 
-let x = 1;
-
-while(x <= 4){
-    console.log(`Iteracion N° ${x}`);
-    x++;
-}
-console.log("Fin de while");
+const numeros = [10,20,30,40];
+console.log(numeros);
+console.table(numeros); 
 
 
-// Ejemplo N°4 (ciclo do while)
+// Acceder a un valor en el arreglo
 
-let y = 1;
+console.log(numeros[1]); 
+console.log(numeros[2]); 
+console.log(numeros[0]);
 
-do{
-    console.log(`Iteracion N° ${y}`);
-    y++;
-}while(y <=4);
+// Saber el largo del array
 
-console.log("Fin del ciclo do while");
+console.log(meses.length);
 
+// Recorrer el largo del arreglo
 
-// Ejemplo N°6 (ciclo for con validacion de numeros de saludos)
-
-let numero = prompt("Cuantas veces desea repetir el saludo?");
-let i = 0;
-
-for(i = 0; i < numero;i++){
-    console.log("hola");
+for(let i=0;i < meses.length;i++){
+    console.log(meses[i]);
 }
 
-// Ejemplo N°7 (switch)
+// Agregar o modificar un elemento de un arreglo
 
-var frase = "";
-var autor = prompt("Ingrese autor");
+meses[0] = 'abril'; // ===> con esto modificamos el indice 0 del arreglo original
+meses[4] = 'mayo'; // ===> con esto agregamos un nuevo mes al final del array
 
-switch(autor){
-    case "darwin":
-        frase = "Sin dudas no hay progreso";
-        break;
-    case "einstein":
-        frase = "Dos cosas son infinitas: la estupidez humana y el universo;y no estoy seguro de lo segundo";
-        break;
-    case "batman":
-        frase = "I belive in Harvey Dent";
-        break;
-    default:
-        frase = "nada que comentar..."       
+console.table(meses);
+
+// Agregar elementos al final del array
+
+meses.push('junio');
+meses.push('julio');
+
+// DESTRUCTURING DE ARRAYS
+
+const numeros2 = [10,20,30,40,50,60]; 
+
+const [, ,primero, ,segundo] = numeros2;
+
+console.log(primero);
+console.log(segundo);
+
+
+
+// PROYECTO CARRITO
+
+const carrito = [];
+
+const producto ={
+    nombre: 'monitor',
+    precio: 30000
 }
 
-alert(frase);
-console.log(frase);
+const producto2={
+    nombre: 'tablet',
+    precio: 40000
+}
+
+carrito.push(producto);
+carrito.push(producto2);
+carrito.unshift(producto2); // ===> con esto enviamos el objeto producto2 al inicio de la tabla
+
+console.table(carrito);
+
+let resultado;
+resultado = [...carrito,producto]; // ===> esto depende del orden en que declaramos los elementos, es como se van a mostrar en en arreglo final de la tabla...
+
+console.table(resultado);
+
+// Eliminar elementos del array con SPLICE()
+
+carrito.splice(1,1); // ===> elimina desde el indice 1 hasta el indice 1 tambien en este caso.
+console.table(carrito);
 
 
 
+// Metodo .forEach()
+const carrito2 = [
+    {nombre:"monitor",precio:1000},
+    {nombre:"teclado",precio:500},
+    {nombre:"mouse",precio:100},
+    {nombre:"parlante",precio:700},
+]
+
+carrito2.forEach(producto =>console.log(`El ${producto.nombre} cuesta ${producto.precio}`));
+
+
+// Metodo .map()
+carrito2.map(producto =>console.log(`El ${producto.nombre} cuesta ${producto.precio}`));
+
+// La sintaxis es la misma que .forEach(),simplemente que .map() crea un nuevo array,lo cual no lo hace .forEach()
+
+
+
+// para saber si un producto esta dentro del carrito uso:
+const existe = carrito2.some(producto =>producto.nombre === 'teclado');
+console.log(existe);
+
+
+// para saber el monto total del carrito uso:
+let resultado2 = carrito2.reduce((total,producto) => total + producto.precio,0);
+console.log();
+
+
+// para saber que productos cumplen con ciertos requisitos use:
+let resultado3;
+resultado3 = carrito2.filter(producto =>producto.precio > 300);
+console.log(resultado3);
+
+
+// para saber las especificaciones de un producto dentro del carrito uso:
+const resultado4 = carrito2.find(producto=>producto.nombre ==='monitor');
+console.log(resultado4);
+
+
+// para saber si todos los productos cumplen con una especificacion uso:
+const resultado5 = carrito2.every(producto => producto.precio < 50);
+console.log(resultado5);
+
+
+
+// para juntar 2 o mas carritos use:
+const carrito3 =[
+    {nombre:'televisor',precio:60000},
+    {nombre:'teclado',precio:5000},
+    {nombre:'luz led',precio:7000}
+];
+
+const carritoTotal = carrito2.concat(carrito3);
+console.log(carritoTotal);
