d344a7ef1035374307f128ee869c898ee84b518e
theopharnell <98174756+theopharnell@users.noreply.github.com>
2022-09-18 13:00:30 -0300

    agregado de archivo typed.js

---
+++ Js/typed.js
@@ -0,0 +1,39 @@
+// TYPEDJS
+
+const typed = new Typed('.typed', {
+    strings: ['+ estilo','+ diseño'],
+    loop: true,
+    typeSpeed: 60,
+    backSpeed: 80,
+    showCursor: false,
+    
+});
+
+const typed2 = new Typed('.typed2', {
+    strings: ['Angular.Js'],
+    loop: true,
+    typeSpeed: 90,
+    backSpeed: 120,
+});
+
+
+const typed9 = new Typed('.typed9', {
+    strings: ['Bulma'],
+    loop: true,
+    typeSpeed: 90,
+    backSpeed: 120,
+});
+
+const typed10 = new Typed('.typed10', {
+    strings: ['TypeScript'],
+    loop: true,
+    typeSpeed: 90,
+    backSpeed: 100,
+});
+const typed11 = new Typed('.typed11', {
+    strings: ['Drupal'],
+    loop: true,
+    typeSpeed: 90,
+    backSpeed: 100,
+});
+
